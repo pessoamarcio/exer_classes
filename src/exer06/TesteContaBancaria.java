c91642528dcd26774272f248df03264c22a1d397
pessoamarcio <103587377+pessoamarcio@users.noreply.github.com>
2023-01-04 10:26:25 -0300

    ajuste

--- src/exer06/TesteContaBancaria.java
+++ src/exer06/TesteContaBancaria.java
@@ -4,20 +4,15 @@ public class TesteContaBancaria {
 
 	public static void main(String[] args) {
 		
-		// aqui estamos instanciando um novo objeto de conta bancaria usando a classe conta bancaria. Usando o m�todo construtor, passamos todos os dados para preecher os atributos dessa nova conta gerada
-		ContaBancaria novaConta = new ContaBancaria(12, 65, 2, "Carol", 1200f);
-		
-		
-		// cada system.out.print esta retornando os dados da nova conta criada, usando os metodos acessores get, lembrando que get � para mostrar os dados, e set � para inserir os dado. 
+		ContaBancaria novaConta = new ContaBancaria(200, 6534, 2, "Maria", 1200f);
+				
 		System.out.println("Numero da Conta  "+ novaConta.getNumero());
 		System.out.println("Numero da Agencia "+ novaConta.getAgencia());
-		System.out.println("Numero do Tipo "+ novaConta.getTipo());
-		System.out.println("Numero da Titular "+ novaConta.getTitular());
-		System.out.println("Numero da Saldo "+ novaConta.getSaldo());
+		System.out.println("Tipo de conta "+ novaConta.getTipo());
+		System.out.println("Nome do titular "+ novaConta.getTitular());
+		System.out.println("Saldo total da conta "+ novaConta.getSaldo());
 		
-		
-		// aqui estamos testando o metodo especifico criado para a classe de conta, que � o sacar, informando diretamente o valor a ser sacado da conta criada.
-		novaConta.sacar(20000);
+		novaConta.sacar(22);
 	
 	}
 
